5025cbd548055b0df9636ba81578a31fbf8bb131
beerent <brentryczak@gmail.com>
2021-11-28 07:40:27 -0500

    implementing tailwind.css

--- App.tsx
+++ App.tsx
@@ -1,12 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import { Link } from 'react-router-dom';
+import tailwind from 'tailwind-rn';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Welcome to embtr. Coming Soon! Follow our progress on <a href="https:www.twitter.com/_embtr">twitter</a>.</Text>
+      <Text>Welcome to embtr. Coming Soon! Follow our progress on <a href="https:www.twitter.com/_embtr">
+        <Text style={tailwind('underline text-blue-600 hover:text-blue-800 visited:text-purple-600')}>
+          Twitter 👋
+        </Text>
+        </a>.
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
