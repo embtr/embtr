ce3fad2ecc76641587a3a33b08493285e223c8d7
beerent <brentryczak@gmail.com>
2021-11-28 08:06:09 -0500

    fixing twitter url

--- App.tsx
+++ App.tsx
@@ -3,6 +3,8 @@ import React from 'react';
 import { StyleSheet, Text, View, Linking } from 'react-native';
 import tailwind from 'tailwind-rn';
 
+const TWITTER_LINK = "https://www.twitter.com/_embtr"
+
 export default function App() {
   return (
     <View style={styles.container}>
@@ -11,7 +13,7 @@ export default function App() {
         {"\n"}
         {"\n"}
         Coming Soon! Follow our progress on{" "}
-        <Text style={tailwind('underline text-blue-600')} onPress={() => Linking.openURL('http://google.com')}>
+        <Text style={tailwind('underline text-blue-600')} onPress={() => Linking.openURL(TWITTER_LINK)}>
           Twitter 🐦
         </Text>.
       </Text>
