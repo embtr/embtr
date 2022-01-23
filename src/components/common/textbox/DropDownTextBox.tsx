cee6ecd80174cd6963b9f2e86de8504d26eb6b8f
beerent <brentryczak@gmail.com>
2022-01-23 18:46:48 -0500

    cleaning drop down textbox

--- src/components/common/textbox/DropDownTextBox.tsx
+++ src/components/common/textbox/DropDownTextBox.tsx
@@ -1,7 +1,9 @@
 import * as React from 'react';
-import { TextStyle, Animated, ViewStyle, TextInput, Text, View } from 'react-native';
+import { TextStyle, Animated, ViewStyle, TextInput, Text, View, ScrollView } from 'react-native';
 import { useTheme } from 'src/components/theme/ThemeProvider';
 import { useRef } from 'react';
+import { HorizontalLine } from 'src/components/common/HorizontalLine';
+import { useFocusEffect } from '@react-navigation/native';
 
 interface Props {
     text: string,
@@ -11,6 +13,8 @@ interface Props {
     placeholder?: string,
     display: boolean
 }
+
+const MAX_DROPDOWN_HEIGHT = 40;
 
 export const DropDownTextBox = ({ text, textSize, onChangeText, onSubmitText, placeholder, display }: Props) => {
     const { colors } = useTheme();
@@ -26,7 +30,8 @@ export const DropDownTextBox = ({ text, textSize, onChangeText, onSubmitText, pl
 
     const [collapsed, setCollapsed] = React.useState(display);
 
-    const commentBoxRef = useRef<TextInput>(null);
+    const scrollViewRef = useRef<ScrollView>(null);
+    const defaultRef = useRef<View>(null);
 
 
     const textWindowAnimatedHeight = useRef(new Animated.Value(0)).current;
@@ -35,7 +40,7 @@ export const DropDownTextBox = ({ text, textSize, onChangeText, onSubmitText, pl
             setCollapsed(true);
         }
 
-        if (result.value == 50) {
+        if (result.value == MAX_DROPDOWN_HEIGHT) {
             setCollapsed(false);
         }
     });
@@ -58,48 +63,48 @@ export const DropDownTextBox = ({ text, textSize, onChangeText, onSubmitText, pl
     };
 
     if (display) {
-        fadeIn(textWindowAnimatedHeight, 50);
-        commentBoxRef.current?.focus();
+        fadeIn(textWindowAnimatedHeight, MAX_DROPDOWN_HEIGHT);
+        scrollViewRef.current?.scrollToEnd();
     } else {
         fadeOut(textWindowAnimatedHeight, 0);
     }
 
+
     return (
-        <View >
-            <Animated.View style={{ height: textWindowAnimatedHeight, alignItems: "flex-end", justifyContent: "center" }}>
-                <TextInput
-                    ref={commentBoxRef}
-                    style={[textStyle, inputStyle, {
-                        width: "90%",
-                        marginRight: "5%",
-                        fontSize: 14,
-                        overflow: "hidden",
-                        paddingTop: collapsed ? 0 : 5,
-                        paddingBottom: collapsed ? 0 : 5,
-                        borderWidth: collapsed ? 0 : 1,
-                        paddingLeft: 15
-                    }]}
-                    onChangeText={(text: string) => { onChangeText(text) }}
-                    onSubmitEditing={() => { onSubmitText() }}
-                    value={display ? text : ""}
-                    placeholder={display ? placeholder : ""}
-                    placeholderTextColor={colors.secondary_text}
-                    autoFocus={true} />
+        <View ref={defaultRef}>
+            <Animated.View style={{ height: textWindowAnimatedHeight, alignItems: "flex-end" }}>
+                <ScrollView style={{ width: "100%", height: 200 }} ref={scrollViewRef}>
 
-                <View style={{ zIndex: 1, position: "absolute", paddingRight:"5%" }}>
-                    {display && <View>
-                        <Text onPress={() => { onSubmitText() }} style={{
-                            fontSize: 16,
-                            color: colors.primary_border,
-                        }}>send   </Text>
-                    </View>}
+                </ScrollView>
+                <View style={{ width: "100%", height: !display ? 0 : "auto", alignItems: "flex-end", justifyContent: "center", overflow: "hidden" }}>
+                    <TextInput
+                        style={[textStyle, inputStyle, {
+                            width: "90%",
+                            marginRight: "5%",
+                            fontSize: 14,
+                            paddingTop: 5,
+                            paddingBottom: 5,
+                            borderWidth: collapsed ? 0 : 1,
+                            paddingLeft: 15
+                        }]}
+                        onChangeText={(text: string) => { onChangeText(text) }}
+                        onSubmitEditing={() => { onSubmitText() }}
+                        value={display ? text : ""}
+                        placeholder={display ? placeholder : ""}
+                        placeholderTextColor={colors.secondary_text}
+                        selectionColor={undefined}
+                        autoFocus={false} />
+
+                    <View style={{ zIndex: 1, position: "absolute", paddingRight: "5%" }}>
+                        {display && <View>
+                            <Text onPress={() => { onSubmitText() }} style={{ fontSize: 16, color: colors.primary_border, }}>send   </Text>
+                        </View>}
+                    </View>
                 </View>
-
-
             </Animated.View>
-
-
-
+            {(display || !collapsed) && <View style={{ paddingTop: 10 }}>
+                <HorizontalLine />
+            </View>}
         </View>
     );
 }
--- src/components/common/timeline/TextCard.tsx
+++ src/components/common/timeline/TextCard.tsx
@@ -4,6 +4,7 @@ import { HorizontalLine } from 'src/components/common/HorizontalLine';
 import { useTheme } from 'src/components/theme/ThemeProvider';
 import { Ionicons } from '@expo/vector-icons';
 import { DropDownTextBox } from 'src/components/common/textbox/DropDownTextBox';
+import { TouchableWithoutFeedback } from 'react-native-gesture-handler';
 
 interface Props {
     staticImage?: ImageSourcePropType,
@@ -55,20 +56,23 @@ export const TextCard = ({ staticImage, httpImage, onTouchImage, name, title, bo
         <View>
             <HorizontalLine />
 
-            <View style={{ height: "auto", marginLeft: 10, marginRight: 10, alignItems: "center" }}>
-                <View style={{ width: "100%" }}><Text style={[bodyTextStyle, { textAlign: "right", color: "gray", fontSize: 12 }]}>Jan 20, 2022</Text></View>
-                {staticImage && <View><Image style={{ width: 45, height: 45 }} source={staticImage} /></View>}
-                {httpImage && <View><TouchableOpacity disabled={!onTouchImage} onPress={() => { onTouchImage!() }} ><Image style={{ width: 45, height: 45, borderRadius: 50 }} source={{ uri: httpImage }} /></TouchableOpacity></View>}
-                <View><Text style={[bodyTextStyle, { padding: 5 }]}>{name}</Text></View>
-                <View>
-                    <Text style={[headerTextStyle, { paddingTop: 5, textAlign: "center" }]}>{title}</Text>
-                </View>
+            <TouchableWithoutFeedback onPressIn={undefined} onPress={() => { alert("touch") }}>
+                <View style={{ height: "auto", marginLeft: 10, marginRight: 10, alignItems: "center" }}>
+                    <View style={{ width: "100%" }}><Text style={[bodyTextStyle, { textAlign: "right", color: "gray", fontSize: 12 }]}>Jan 20, 2022</Text></View>
+                    {staticImage && <View><Image style={{ width: 45, height: 45 }} source={staticImage} /></View>}
+                    {httpImage && <View><TouchableOpacity disabled={!onTouchImage} onPress={() => { onTouchImage!() }} ><Image style={{ width: 45, height: 45, borderRadius: 50 }} source={{ uri: httpImage }} /></TouchableOpacity></View>}
+                    <View><Text style={[bodyTextStyle, { padding: 5 }]}>{name}</Text></View>
+                    <View>
+                        <Text style={[headerTextStyle, { paddingTop: 5, textAlign: "center" }]}>{title}</Text>
+                    </View>
 
-                <View style={{ marginTop: 15, marginBottom: 5 }}>
-                    <Text style={[bodyTextStyle, { paddingLeft: "2.5%", paddingRight: "2.5%", textAlign: "left" }]}>{body}</Text>
-                    <Text style={[bodyTextStyle, { color: "gray", fontSize: 12, textAlign: "right", marginTop: 5 }]}>view more...</Text>
+                    <View style={{ marginTop: 15, marginBottom: 5 }}>
+                        <Text style={[bodyTextStyle, { paddingLeft: "2.5%", paddingRight: "2.5%", textAlign: "left" }]}>{body}</Text>
+                        <Text style={[bodyTextStyle, { color: "gray", fontSize: 12, textAlign: "right", marginTop: 5 }]}>view more...</Text>
+                    </View>
                 </View>
-            </View>
+            </TouchableWithoutFeedback>
+
 
             <View style={{ paddingRight: 15, height: "auto", marginTop: 5, marginBottom: 5, flexDirection: "row", justifyContent: "flex-end" }}>
 
@@ -85,13 +89,12 @@ export const TextCard = ({ staticImage, httpImage, onTouchImage, name, title, bo
                 </View>
 
                 <View style={{ borderColor: colors.text, marginLeft: 5 }}>
-                    <Ionicons name={'chatbox-outline'} size={22} color={colors.text} onPress={() => { setDisplayCommentBox(!displayCommentBox); }} />
+                    <Ionicons name={displayCommentBox ? 'chatbox' : 'chatbox-outline'} size={22} color={colors.text} onPress={() => { setDisplayCommentBox(!displayCommentBox); }} />
                 </View>
             </View>
 
-            <DropDownTextBox text={commentText} textSize={14} onChangeText={setCommentText} placeholder='add a comment...' display={displayCommentBox} onSubmitText={onSubmitText} />
-
             <HorizontalLine />
+            <DropDownTextBox text={commentText} textSize={14} onChangeText={setCommentText} placeholder='add a comment...' display={displayCommentBox} onSubmitText={onSubmitText} />
         </View>
     );
 }
