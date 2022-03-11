661456199f55090dcfc0e8ce427a25328e0bede9
beerent <brentryczak@gmail.com>
2022-03-11 05:51:49 -0500

    comments now display users to tag

--- src/components/common/comments/user_tags/UserTagBox.tsx
+++ src/components/common/comments/user_tags/UserTagBox.tsx
@@ -1,17 +1,29 @@
-import { getAuth } from 'firebase/auth';
 import * as React from 'react';
-import { Text, View } from 'react-native';
+import { getAuth } from 'firebase/auth';
+import { View } from 'react-native';
 import { UserTag } from 'src/components/common/comments/user_tags/UserTag';
 import { useTheme } from 'src/components/theme/ThemeProvider';
 import ProfileController from 'src/controller/profile/ProfileController';
 import { UserProfileModel } from 'src/firebase/firestore/profile/ProfileDao';
+import { UserSearchUtility } from 'src/util/user/UserSearchUtility';
+import { UsernameTagTracker } from 'src/util/user/UsernameTagTracker';
+import UserSearchResultObject from 'src/firebase/firestore/user/UserSearchResultObject';
 
 interface Props {
     input: string
 }
 
+
+
 export const UserTagBox = ({ input }: Props) => {
     const { colors } = useTheme();
+
+    const [userSearchUtility, setUserSearchUtility] = React.useState<UserSearchUtility | undefined>(undefined);
+    const [usernameTagOptions, setUsernameTagOptions] = React.useState<UserProfileModel[]>([]);
+
+    React.useEffect(() => {
+        setUserSearchUtility(new UserSearchUtility());
+    }, []);
 
     const [user, setUser] = React.useState<UserProfileModel>();
     const [display, setDisplay] = React.useState<boolean>(false);
@@ -20,25 +32,40 @@ export const UserTagBox = ({ input }: Props) => {
         ProfileController.getProfile(getAuth().currentUser!.uid, setUser);
     }, []);
 
-    if (!display && input.charAt(input.length - 1) == "@") {
+    React.useEffect(() => {
+        const username = UsernameTagTracker.getUsernameFromText(input);
+        if (username) {
+            userSearchUtility?.updateSearch(username, (results: UserSearchResultObject) => {
+                if (results?.results) {
+                    setUsernameTagOptions(results.results);
+                }
+            });
+        }
+    }, [input]);
 
+    const isTypingUsername = UsernameTagTracker.isTypingUsername(input);
+
+    if (!display && isTypingUsername) {
         setDisplay(true);
+    } else if (display && !isTypingUsername) {
+        setDisplay(false);
+    }
+
+    let usernameTagOptionsViews: JSX.Element[] = [];
+    if (usernameTagOptions) {
+        usernameTagOptions.forEach(usernameTagOption => {
+            usernameTagOptionsViews.push(<UserTag userProfile={usernameTagOption} />);
+        });
+
+        console.log(usernameTagOptionsViews)
     }
 
     return (
-        display
-            ?
-            (
-                <View style={{ padding: 1, backgroundColor: colors.background_secondary, borderWidth: 1, borderColor: colors.background,borderRadius: 5 }} >
-                    {user && <UserTag userProfile={user} />}
-                    {user && <UserTag userProfile={user} />}
-                    {user && <UserTag userProfile={user} />}
-                    {user && <UserTag userProfile={user} />}
-                </View >
-            )
+        display ?
+            (<View style={{ padding: 1, backgroundColor: colors.background_secondary, borderWidth: 1, borderColor: colors.background, borderRadius: 5 }} >
+                {usernameTagOptionsViews}
+            </View >)
             :
-            (
-                <View />
-            )
+            (<View />)
     )
 }
---
+++ src/util/user/UsernameTagTracker.tsx
@@ -0,0 +1,21 @@
+export class UsernameTagTracker {
+    public static isTypingUsername(text: string): boolean {
+        const lastAtIndex = text.lastIndexOf("@");
+        if (lastAtIndex < 0) {
+            return false;
+        }
+
+        const remainder = text.substring(lastAtIndex + 1);
+        return remainder.indexOf(" ") < 0;
+    }
+
+    public static getUsernameFromText(text: string): string | undefined {
+        const lastAtIndex = text.lastIndexOf("@");
+        if (lastAtIndex < 0) {
+            return undefined;
+        }
+
+        const remainder = text.substring(lastAtIndex + 1);
+        return remainder;
+    }
+}
