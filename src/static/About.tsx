13928ff344e62c4b62cde0bb239726c35d70b62d
beerent <brentryczak@gmail.com>
2022-03-17 20:03:22 -0400

    updated the about page

--- src/static/About.tsx
+++ src/static/About.tsx
@@ -44,7 +44,7 @@ export const About = () => {
                     <Text>{"\n\n"}</Text>
                     <Text style={textStyle}>Welcome to embtr! We have been on a life-long journey to dial in our habits and maximize our daily output towards what we find most important. We believe there are two core components to a highly successful work process; planning and accountability. These two concepts live at the core of embtr. You can think of embtr. as a life organizer that helps you identify and execute the most productive routines that are geared towards improving the areas of life that you find most important. embtr. is community-based so you will be grinding alongside other go-getters just like you. The community helps holds you accountable and keeps you motivated. We're all on this journey together; let's share our wins, our losses, and our lessons learned. The good, the bad, and the ugly. These are the ingredients that make up a well seasoned and highly effective individual.</Text>
                     <Text>{"\n\n"}</Text>
-                    <Text style={textStyle}>Come on inside and see what the journey to productivity is all about. We look forward to it!</Text>
+                    <Text style={textStyle}>Come on inside and see what the journey to productivity is all about!</Text>
                     <Text>{"\n\n\n\n"}</Text>
                     <View style={{ justifyContent: 'center', alignItems: 'center' }}>
                         <Button title='home' onPress={() => { navigation.navigate('LandingPage') }}></Button>
