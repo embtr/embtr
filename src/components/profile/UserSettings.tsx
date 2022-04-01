2599aefda01397c9de59f2825da3985b13045be0
nicklona17 <nicklona17@gmail.com>
2022-03-31 21:18:55 -0400

    moved settings to middle of screen

--- src/components/profile/UserSettings.tsx
+++ src/components/profile/UserSettings.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { View, Button } from 'react-native';
+import { View, Button, Text } from 'react-native';
 import { Screen } from 'src/components/common/Screen';
 import { Banner } from 'src/components/common/Banner';
 import { ThemeToggle } from 'src/components/theme/ThemeToggle';
@@ -14,9 +14,12 @@ export const UserSettings = () => {
         <Screen>
             <Banner name='Settings' leftIcon={"arrow-back"} leftRoute="BACK" />
 
-            <View style={{ flexDirection: "column", flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-                <ThemeToggle />
-                <View style={{ flex: 2, justifyContent: 'center', alignItems: 'center' }}>
+            <View style={{ flex: 1, flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
+                <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                    <ThemeToggle />
+                    <Text style={{ color: 'grey' }}>     Light Mode/Dark Mode</Text>  
+                </View>
+                <View>
                     <Button title='logout' onPress={() => { getAuth().signOut(); dispatch(setAccessLevel("invalid")); }}></Button>
                 </View>
             </View>
