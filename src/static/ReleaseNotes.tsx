62d547cd88042dae88ba7d6c0ace5e8c3e453b37
nicklona17 <nicklona17@gmail.com>
2022-04-05 15:35:21 -0400

    organized version history and turned heart logo into home button

--- src/static/ReleaseNotes.tsx
+++ src/static/ReleaseNotes.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { StackNavigationProp } from "@react-navigation/stack";
-import { View, Text, TextStyle, ViewStyle, Image, Button } from "react-native";
+import { TouchableOpacity, View, Text, TextStyle, ViewStyle, Image, Button, StyleSheet } from "react-native";
 import { Screen } from 'src/components/common/Screen';
 import { useTheme } from "src/components/theme/ThemeProvider";
 import { RootStackParamList } from "src/navigation/RootStackParamList";
@@ -22,29 +22,56 @@ export const ReleaseNotes = () => {
         }, [])
     );
 
+    
     let releaseNotesView: JSX.Element[] = [];
+    let orderedReleaseNotes = releaseNotes.sort((a, b) => (a.version > b.version) ? 1 : -1).reverse();
+    
     releaseNotes.forEach(releaseNote => {
-        console.log(releaseNote);
+        const notes = releaseNote.notes;
+        
+        const bulletedNotes = notes.map((notes) => <li>{notes}</li>);
         releaseNotesView.push(
             <View key={releaseNote.id}>
                 <Text style={{ color: colors.text }}>{releaseNote.version}</Text>
-                <Text style={{ color: colors.text }}>{releaseNote.notes}</Text>
+                <ul>
+                    <Text style={{ color: colors.text }}>{bulletedNotes}</Text>
+                </ul>
             </View>
         );
     });
-
+    
+    
     return (
         <Screen>
-            <View style={{ alignContent: "center", alignItems: "center", flex: 1 }}>
-                <Text style={{ color: colors.text, fontSize: 24 }}>Release Notes</Text>
-                <Image source={require('../../assets/logo.png')} style={{ width: 200, height: 200 }} />
+            <View style={styles.container}>
+                <Text style={{ color: colors.text, fontSize: 22 }}>
+                    Release Notes
+                </Text>
                 <View>
                     {releaseNotesView}
                 </View>
-            </View>
-            <View style={{ justifyContent: 'center', alignItems: 'center' }}>
-                <Button title='home' onPress={() => { navigation.navigate('LandingPage') }}></Button>
+                <TouchableOpacity style={styles.button} onPress={() => { navigation.navigate('LandingPage') }}>
+                    <Image source={require('../../assets/logo.png')} style={{ width: 40, height: 40 }} />
+                    <Text style={{ color: colors.text, fontSize: 12 }}>
+                        home
+                    </Text>
+                </TouchableOpacity>
             </View>
         </Screen>
-    )
-};
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "space-evenly",
+        flexDirection: "column"
+    },
+    button: {
+        alignItems: "center",
+        justifyContent: "center",
+        flexDirection: "column",
+        cursor: "pointer"
+    }
+});
