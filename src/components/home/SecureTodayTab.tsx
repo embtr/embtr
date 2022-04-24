cd4c0d06441cd774187622b680aed2819c184f87
beerent <brentryczak@gmail.com>
2022-04-24 10:06:30 -0700

    creating initial today page

--- src/components/home/SecureTodayTab.tsx
+++ src/components/home/SecureTodayTab.tsx
@@ -1,8 +1,14 @@
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import React from 'react';
 import { View, Text } from 'react-native';
+import { Today } from 'src/components/today/Today';
 
 export const SecureTodayTabStack = () => {
+    const Stack = createNativeStackNavigator();
+
     return (
-        <View><Text>hailo, today tab!</Text></View>
+        <Stack.Navigator initialRouteName='Today' screenOptions={{ headerShown: false }}>
+            <Stack.Screen name="Today" component={Today} />
+        </Stack.Navigator>
     );
 };
---
+++ src/components/today/Today.tsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import { useFocusEffect, useNavigation } from '@react-navigation/native';
+import { StackNavigationProp } from '@react-navigation/stack';
+import { getAuth } from 'firebase/auth';
+import { View } from 'react-native';
+import { ScrollView } from 'react-native-gesture-handler';
+import { AddButton } from 'src/components/common/button/AddButton';
+import { useTheme } from 'src/components/theme/ThemeProvider';
+import TaskController, { TaskModel } from 'src/controller/planning/TaskController';
+import { PlanTabScreens } from 'src/navigation/RootStackParamList';
+import { Task } from 'src/components/plan/tasks/Task';
+import { Screen } from 'src/components/common/Screen';
+
+export const Today = () => {
+    const { colors } = useTheme();
+    const navigation = useNavigation<StackNavigationProp<PlanTabScreens>>();
+
+    const [tasks, setTasks] = React.useState<TaskModel[]>([]);
+
+    useFocusEffect(
+        React.useCallback(() => {
+            TaskController.getTasks(getAuth().currentUser!.uid, setTasks);
+        }, [])
+    );
+
+    const getVisibleTasks = (): TaskModel[] => {
+        let visibleTasks: TaskModel[] = [];
+        tasks.forEach(task => {
+            visibleTasks.push(task);
+        });
+
+        return visibleTasks;
+    };
+
+    const visibleTasks = getVisibleTasks();
+
+    let taskViews: JSX.Element[] = [];
+    visibleTasks.forEach(task => {
+        taskViews.push(
+            <Task key={task.id} task={task} />
+        );
+    });
+
+    return (
+        <Screen>
+            <View style={{ height: "100%" }}>
+                <ScrollView style={{ backgroundColor: colors.background_medium }}>
+                    {taskViews}
+                </ScrollView>
+
+                <View style={{ position: "absolute", right: 0, bottom: 0 }}>
+                    <AddButton onPress={() => { navigation.navigate('CreateTask') }} />
+                </View>
+            </View>
+        </Screen>
+    );
+};
