ab3ed3dff05d1af27924516ad7326b71e984388b
beerent <brentryczak@gmail.com>
2022-06-21 05:42:30 -0400

    fixing alignment

--- src/components/profile/search/UserSearchResult.tsx
+++ src/components/profile/search/UserSearchResult.tsx
@@ -35,13 +35,13 @@ export const UserSearchResult = ({ userProfileModel, onFollowUser, onUnfollowUse
     return (
         <View style={{ width: "100%", alignItems: "center" }}>
             <TouchableOpacity style={{ width: USER_SEARCH_WIDTH }} onPress={() => { navigation.navigate('UserProfile', { id: userProfileModel.uid! }) }}>
-                <View style={{ backgroundColor: colors.button_background, alignItems: "center", height: 75, borderRadius: 15, width: "100%", flexDirection: "row", paddingLeft: 10 }}>
+                <View style={{ backgroundColor: colors.button_background, alignItems: "center", height: 75, borderRadius: 15, width: "100%", flexDirection: "row", paddingLeft: 20 }}>
                     <View style={{ flex: 2, flexDirection: "row", alignItems: "center" }}>
                         <NavigatableUserImage userProfileModel={userProfileModel} size={35} />
                         <Text style={{ fontFamily: "Poppins_500Medium", fontSize: 15, color: colors.user_search_name, paddingLeft: 10 }}>{userProfileModel?.name}</Text>
                     </View>
 
-                    <View style={{ flex: 1, alignContent: "center", alignItems: "center" }}>
+                    <View style={{ flex: 1, alignContent: "center", alignItems: "flex-end", paddingRight: 15 }}>
                         <FollowUserButton userProfileModel={userProfileModel} onFollowUser={onFollowUser} onUnfollowUser={onUnfollowUser} following={following} />
                     </View>
                 </View>
