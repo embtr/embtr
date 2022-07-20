4e79ba890545f9e31ec18cc976773b541a1dd22a
beerent <brentryczak@gmail.com>
2022-07-20 05:12:00 -0400

    fixing time indicator position

--- src/components/today/views/calendar/time_indicator/TimeIndicator.tsx
+++ src/components/today/views/calendar/time_indicator/TimeIndicator.tsx
@@ -3,6 +3,7 @@ import { useFocusEffect } from '@react-navigation/native';
 import { View } from 'react-native';
 import { TimeIndicatorDot } from 'src/components/today/views/calendar/time_indicator/TimeIndicatorDot';
 import { TimeIndicatorLine } from 'src/components/today/views/calendar/time_indicator/TimeIndicatorLine';
+import { CALENDAR_TIME_HEIGHT } from 'src/util/constants';
 
 export const TimeIndicator = () => {
 
@@ -18,7 +19,7 @@ export const TimeIndicator = () => {
         }, [minutes])
     );
 
-    const height = hours * 60 + minutes;
+    const height = hours * 60 + minutes + (CALENDAR_TIME_HEIGHT/2);
     return (
         <View style={{ flexDirection: "row", zIndex: 1, position: "absolute", width: "100%" }}>
             <TimeIndicatorLine height={height} />
