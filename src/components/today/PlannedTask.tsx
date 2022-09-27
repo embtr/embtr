b5c583e3d974afe5e074e7b8dfd3bce563a4a356
beerent <brentryczak@gmail.com>
2022-09-27 04:53:47 -0400

    cleaning

--- src/components/today/PlannedTask.tsx
+++ src/components/today/PlannedTask.tsx
@@ -1,35 +1,27 @@
-import { useNavigation } from '@react-navigation/native';
-import React from 'react';
-import { StackNavigationProp } from '@react-navigation/stack';
-import { View, Text, ColorValue, TouchableOpacity } from 'react-native';
+import { View, Text, ColorValue } from 'react-native';
 import { useTheme } from 'src/components/theme/ThemeProvider';
 import { PlannedTaskModel } from 'src/controller/planning/PlannedDayController';
-import { durationToString, TaskModel, startMinuteToString } from 'src/controller/planning/TaskController';
-import { PlanTabScreens } from 'src/navigation/RootStackParamList';
+import { durationToString, startMinuteToString } from 'src/controller/planning/TaskController';
 
 interface Props {
-    plannedTask: PlannedTaskModel,
-    backgroundColor?: ColorValue
+    plannedTask: PlannedTaskModel;
+    backgroundColor?: ColorValue;
 }
 
 export const PlannedTask = ({ plannedTask, backgroundColor }: Props) => {
     const { colors } = useTheme();
 
-    const navigation = useNavigation<StackNavigationProp<PlanTabScreens>>();
-
     return (
         <View style={{ backgroundColor: backgroundColor || colors.card_background_active }}>
-            <View style={{ height: "auto", paddingTop: 5, paddingBottom: 5 }}>
+            <View style={{ height: 'auto', paddingTop: 5, paddingBottom: 5 }}>
                 <View style={{ padding: 5 }}>
-                    <Text style={{ color: colors.text, textAlign: "center" }}>
-                        {plannedTask.routine.name}
-                    </Text>
+                    <Text style={{ color: colors.text, textAlign: 'center' }}>{plannedTask.routine.name}</Text>
                 </View>
 
-                <Text style={{ color: colors.text, textAlign: "center", paddingTop: 5 }}>
-                    {startMinuteToString(plannedTask.startMinute ? plannedTask.startMinute : 0)
-                        + " for "
-                        + durationToString(plannedTask.duration ? plannedTask.duration : 0)}
+                <Text style={{ color: colors.text, textAlign: 'center', paddingTop: 5 }}>
+                    {startMinuteToString(plannedTask.startMinute ? plannedTask.startMinute : 0) +
+                        ' for ' +
+                        durationToString(plannedTask.duration ? plannedTask.duration : 0)}
                 </Text>
                 <View style={{ flex: 1 }} />
             </View>
