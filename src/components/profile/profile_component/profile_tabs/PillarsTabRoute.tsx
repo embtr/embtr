f6edca642febf49427ebbe76dce8a55efe6de667
beerent <brentryczak@gmail.com>
2022-12-02 04:35:25 -0500

    cleaning

--- src/components/profile/profile_component/profile_tabs/PillarsTabRoute.tsx
+++ src/components/profile/profile_component/profile_tabs/PillarsTabRoute.tsx
@@ -1,32 +1,15 @@
 import { Pillars } from 'src/components/profile/profile_component/pillar/Pillars';
-import { View, Button } from 'react-native';
+import { View } from 'react-native';
 import { UserProfileModel } from 'src/firebase/firestore/profile/ProfileDao';
-import { useNavigation } from '@react-navigation/native';
-import { getAuth } from 'firebase/auth';
-import { StackNavigationProp } from '@react-navigation/stack';
-import { ProfileTabScreens } from 'src/navigation/RootStackParamList';
 
 interface Props {
     userProfileModel: UserProfileModel;
 }
 
 function PillarsTabRoute({ userProfileModel }: Props) {
-    const navigation = useNavigation<StackNavigationProp<ProfileTabScreens, 'PillarsConfiguration'>>();
-    const isCurrentUser = getAuth().currentUser!.uid === userProfileModel.uid;
-
     return (
         <View>
             <Pillars userProfileModel={userProfileModel} />
-            {isCurrentUser && (
-                <View style={{ paddingTop: 20 }}>
-                    <Button
-                        title="configure"
-                        onPress={() => {
-                            navigation.navigate('PillarsConfiguration');
-                        }}
-                    ></Button>
-                </View>
-            )}
         </View>
     );
 }
