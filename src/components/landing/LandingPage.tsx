e1554852cd7e34ae4cb8cb28742c49f973628d7e
beerent <brent@embtr.com>
2023-04-23 06:07:16 -0400

    fixed landing page

--- src/components/landing/LandingPage.tsx
+++ src/components/landing/LandingPage.tsx
@@ -2,7 +2,6 @@ import * as React from 'react';
 import { Text, TextStyle, Image, View, ViewStyle } from 'react-native';
 import { Screen } from 'src/components/common/Screen';
 import { useTheme } from 'src/components/theme/ThemeProvider';
-import { isDesktopBrowser } from 'src/util/DeviceUtil';
 import { FirebaseAuthenticate } from 'src/components/login/google/FirebaseAuthenticate';
 import { POPPINS_REGULAR } from 'src/util/constants';
 import { EmbtrButton } from '../common/button/EmbtrButton';
@@ -20,10 +19,6 @@ export const LandingPage = () => {
         fontFamily: 'Poppins_400Regular',
     } as TextStyle;
 
-    const textViewStyle = {
-        width: isDesktopBrowser() ? '60%' : '95%',
-    } as ViewStyle;
-
     const [displayLoginModal, setDisplayLoginModal] = React.useState(false);
     const [displayRegisterModal, setDisplayRegisterModal] = React.useState(false);
 
@@ -38,7 +33,11 @@ export const LandingPage = () => {
     return (
         <Screen>
             <ModalContainingComponent modalVisible={displayLoginModal || displayRegisterModal} />
-            <LoginModal visible={displayLoginModal} confirm={onLoginModalConfirm} dismiss={onLoginModalCancel} />
+            <LoginModal
+                visible={displayLoginModal}
+                confirm={onLoginModalConfirm}
+                dismiss={onLoginModalCancel}
+            />
             <RegisterModal
                 visible={displayRegisterModal}
                 onDismiss={() => {
@@ -50,17 +49,25 @@ export const LandingPage = () => {
                 {/* FLEX 1 LOGO */}
                 <View style={{ flex: 1, alignItems: 'center', justifyContent: 'flex-end' }}>
                     <View style={{ bottom: '10%' }}>
-                        <Image source={require('assets/logo.png')} style={{ width: 200, height: 200 }} />
+                        <Image
+                            source={require('assets/logo.png')}
+                            style={{ width: 200, height: 200 }}
+                        />
                     </View>
-                    <Image source={require('assets/logo_text.png')} style={{ width: 150, height: 50 }} />
+                    <Image
+                        source={require('assets/logo_text.png')}
+                        style={{ width: 150, height: 50 }}
+                    />
                 </View>
 
                 {/* FLEX 3 BUTTONS*/}
                 <View style={{ flex: 1 }}>
                     {/* FLEX 2 TEXY*/}
                     <View style={{ flex: 1 }}>
-                        <View style={[textViewStyle, { flex: 1, justifyContent: 'center' }]}>
-                            <Text style={textStyle}>A community achieving their wildest dreams.</Text>
+                        <View style={{ flex: 1, justifyContent: 'center' }}>
+                            <Text style={textStyle}>
+                                A community achieving their wildest dreams.
+                            </Text>
                             <Text style={textStyle}>Together.</Text>
                         </View>
 
@@ -79,7 +86,16 @@ export const LandingPage = () => {
                                 />
                             </View>
 
-                            <Text style={{ color: colors.text, textAlign: 'center', fontFamily: POPPINS_REGULAR, paddingTop: 20 }}>New to embtr? Sign up.</Text>
+                            <Text
+                                style={{
+                                    color: colors.text,
+                                    textAlign: 'center',
+                                    fontFamily: POPPINS_REGULAR,
+                                    paddingTop: 20,
+                                }}
+                            >
+                                New to embtr? Sign up.
+                            </Text>
                             <View style={{ width: 300, paddingTop: 6 }}>
                                 <EmbtrButton
                                     color="#b50017"
