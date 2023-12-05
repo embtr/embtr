828901dab09fb8a9b4831a28deffb20204acc061
beerent <brentryczak@gmail.com>
2023-12-05 15:23:52 -0500

    Remove unnecessary console logs in OptimalImage component

--- src/components/common/images/OptimalImage.tsx
+++ src/components/common/images/OptimalImage.tsx
@@ -1,6 +1,6 @@
 import { Image, ImageStyle, StyleProp, View } from 'react-native';
 import { LocalImageRepo } from './LocalImageRepo';
-import { Image as ExpoImage, ImageBackground } from 'expo-image';
+import { Image as ExpoImage } from 'expo-image';
 import { isIosApp } from 'src/util/DeviceUtil';
 
 export interface OptimalImageData {
@@ -16,16 +16,13 @@ interface Props {
 export const OptimalImage = ({ data, style }: Props) => {
     if (data.localImage) {
         if (isIosApp()) {
-            console.log('loading ios local image', data.localImage);
             return <ExpoImage source={LocalImageRepo.get(data.localImage)} style={style} />;
         } else {
-            console.log('loading android local image', data.localImage);
             return <Image source={LocalImageRepo.get(data.localImage)} style={style} />;
         }
     }
 
     if (data.remoteImageUrl) {
-        console.log('loading remote image', data.remoteImageUrl);
         return <ExpoImage source={{ uri: data.remoteImageUrl }} style={style} />;
     }
 
