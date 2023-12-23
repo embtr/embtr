e47b1dd3f95629deae99e5b1bf6b679a59e4ccd3
beerent <brentryczak@gmail.com>
2023-12-23 08:35:18 -0500

    cleaned up quote of the day

--- src/controller/widget/quote_of_the_day/QuoteOfTheDayController.tsx
+++ src/controller/widget/quote_of_the_day/QuoteOfTheDayController.tsx
@@ -1,12 +1,21 @@
-import { CreateQuoteOfTheDayRequest } from 'resources/types/requests/QuoteOfTheDayTypes';
+import {
+    CreateQuoteOfTheDayRequest,
+    GetQuoteOfTheDayResponse,
+} from 'resources/types/requests/QuoteOfTheDayTypes';
 import axiosInstance from 'src/axios/axios';
+import { useQuery } from '@tanstack/react-query';
+import { ReactQueryStaleTimes } from 'src/util/constants';
+import PlannedDayController from 'src/controller/planning/PlannedDayController';
+import { QuoteOfTheDay } from 'resources/schema';
+import { reactQueryClient } from 'src/react_query/ReactQueryClient';
 
 export class QuoteOfTheDayController {
-    public static async get() {
+    public static async get(): Promise<QuoteOfTheDay> {
         return await axiosInstance
             .get(`/quote-of-the-day/`)
             .then((success) => {
-                return success.data.quoteOfTheDay;
+                const response: GetQuoteOfTheDayResponse = success.data;
+                return response.quoteOfTheDay;
             })
             .catch((error) => {
                 return error.response.data;
@@ -24,4 +33,27 @@ export class QuoteOfTheDayController {
                 return error.response.data;
             });
     }
+
+    public static async prefetchQuoteOfTheDay() {
+        reactQueryClient.prefetchQuery({
+            queryKey: ['quoteOfTheDay'],
+            queryFn: () => QuoteOfTheDayController.get(),
+            staleTime: ReactQueryStaleTimes.INSTANTLY,
+        });
+    }
+
+    public static async invalidateQuoteOfTheDay() {
+        await reactQueryClient.invalidateQueries(['quoteOfTheDay']);
+    }
+}
+export namespace QuoteOfTheDayCustomHooks {
+    export const useQuoteOfTheDay = () => {
+        const { status, error, data, fetchStatus } = useQuery({
+            queryKey: ['quoteOfTheDay'],
+            queryFn: () => QuoteOfTheDayController.get(),
+            staleTime: ReactQueryStaleTimes.INSTANTLY,
+        });
+
+        return { isLoading: status === 'loading' && fetchStatus !== 'idle', data };
+    };
 }
