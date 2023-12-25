4fa376ac253787453c4e88d123f789aa131894fd
stronkbad <37816659+stronkbad@users.noreply.github.com>
2023-12-25 10:02:51 -0500

    formatted quoteoftheday

--- src/components/widgets/quote_of_the_day/AddQuoteOfTheDay.tsx
+++ src/components/widgets/quote_of_the_day/AddQuoteOfTheDay.tsx
@@ -10,12 +10,7 @@ import { StackNavigationProp } from '@react-navigation/stack';
 import { TodayTab } from 'src/navigation/RootStackParamList';
 import { QuoteOfTheDayController } from 'src/controller/widget/quote_of_the_day/QuoteOfTheDayController';
 import { isIosApp } from 'src/util/DeviceUtil';
-import {
-    Platform,
-    TouchableWithoutFeedback,
-    Button,
-    Keyboard,
-  } from 'react-native';
+import { TouchableWithoutFeedback, Keyboard } from 'react-native';
 
 export const AddQuoteOfTheDay = () => {
     const { colors } = useTheme();
@@ -42,60 +37,91 @@ export const AddQuoteOfTheDay = () => {
                 rightColor={quote.length > 0 ? colors.link : 'gray'}
             />
 
-        <KeyboardAvoidingView style={{ flex: 1 }} behavior={isIosApp() ? 'padding' : 'height'}>
-        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-        <View style={{ padding: 20,  flex: 1 }}>
-        {/* <View style={{ padding: 24, flex: 1, justifyContent: 'space-around' }}> 
-         Justify content will add a ton of whitespace around each view which will collapse the view. */}
-            <View style={{paddingBottom: 40}}>
-            <View style={{ backgroundColor:'#282828', borderRadius: 5}}>
-                <Text style={{ fontFamily: POPPINS_REGULAR, color: colors.text, fontSize: 13, padding: TIMELINE_CARD_PADDING }}>
-                    Add a quote that inspires you! Once a day we randomly select a
-                quote to inspire the entire community.{' '}
-                <Text style={{ color: colors.accent_color_light, paddingTop: 5 }}>
-                    Yours could be next!
-                    </Text>
-            </Text>
-            </View>
-            </View>
+            <KeyboardAvoidingView style={{ flex: 1 }} behavior={isIosApp() ? 'padding' : 'height'}>
+                <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+                    <View style={{ padding: 20, flex: 1 }}>
+                        {/* <View style={{ padding: 24, flex: 1, justifyContent: 'space-around' }}> 
+                        Justify content will add a ton of whitespace around each view which will collapse the view. */}
+                        <View style={{ paddingBottom: 40 }}>
+                            <View style={{ backgroundColor: '#282828', borderRadius: 5 }}>
+                                <Text
+                                    style={{
+                                        fontFamily: POPPINS_REGULAR,
+                                        color: colors.text,
+                                        fontSize: 13,
+                                        padding: TIMELINE_CARD_PADDING,
+                                    }}
+                                >
+                                    Add a quote that inspires you! Once a day we randomly select a
+                                    quote to inspire the entire community.{' '}
+                                    <Text
+                                        style={{ color: colors.accent_color_light, paddingTop: 5 }}
+                                    >
+                                        Yours could be next!
+                                    </Text>
+                                </Text>
+                            </View>
+                        </View>
 
-            <View style={{flexDirection:'row'}}>
-                    <Text style={{ fontFamily: POPPINS_REGULAR, color: colors.text }} >
-                    Quote
-                    </Text>
-                    {quote.length < 1 && (
-                        <Text style={{ color: colors.tab_selected, paddingLeft: TIMELINE_CARD_PADDING, fontFamily: POPPINS_REGULAR, fontSize: 10, textAlignVertical:'center' }}>
-                            cannot be blank
-                        </Text>
-                    )}
-            </View>
-            
-            <View style={{ paddingBottom: 20}}>
-                <TextInput textAlignVertical="top"
-                    style={{ marginTop: 3, height: 120, borderRadius: 5, backgroundColor: '#282828', color: colors.text, padding: TIMELINE_CARD_PADDING / 2}}
-                        multiline={true}
-                        placeholder={'add a quote that inspires you!'}
-                        placeholderTextColor={colors.secondary_text}
-                        onChangeText={setQuote}
-                        value={quote}/>
-            </View>
+                        <View style={{ flexDirection: 'row' }}>
+                            <Text style={{ fontFamily: POPPINS_REGULAR, color: colors.text }}>
+                                Quote
+                            </Text>
+                            {quote.length < 1 && (
+                                <Text
+                                    style={{
+                                        color: colors.tab_selected,
+                                        paddingLeft: TIMELINE_CARD_PADDING,
+                                        fontFamily: POPPINS_REGULAR,
+                                        fontSize: 10,
+                                        textAlignVertical: 'center',
+                                    }}
+                                >
+                                    cannot be blank
+                                </Text>
+                            )}
+                        </View>
 
-            <View style={{paddingTop: 10 }}>
-                <Text style={{ fontFamily: POPPINS_REGULAR, color: colors.text }}>
-                    Author
-                </Text>
-                <TextInput style={{ marginTop: 3, padding: TIMELINE_CARD_PADDING, color: colors.text, borderRadius: 5, backgroundColor: '#282828' }}
-                                        placeholder={"Author's Name (Optional)"}
-                                        placeholderTextColor={colors.secondary_text}
-                                        onChangeText={setAuthor}
-                                        value={author}/>
-            </View>
-                       
-   
-        </View>
-        </TouchableWithoutFeedback>
-        </KeyboardAvoidingView>
+                        <View style={{ paddingBottom: 20 }}>
+                            <TextInput
+                                textAlignVertical="top"
+                                style={{
+                                    marginTop: 3,
+                                    height: 120,
+                                    borderRadius: 5,
+                                    backgroundColor: '#282828',
+                                    color: colors.text,
+                                    padding: TIMELINE_CARD_PADDING / 2,
+                                }}
+                                multiline={true}
+                                placeholder={'add a quote that inspires you!'}
+                                placeholderTextColor={colors.secondary_text}
+                                onChangeText={setQuote}
+                                value={quote}
+                            />
+                        </View>
 
+                        <View style={{ paddingTop: 10 }}>
+                            <Text style={{ fontFamily: POPPINS_REGULAR, color: colors.text }}>
+                                Author
+                            </Text>
+                            <TextInput
+                                style={{
+                                    marginTop: 3,
+                                    padding: TIMELINE_CARD_PADDING,
+                                    color: colors.text,
+                                    borderRadius: 5,
+                                    backgroundColor: '#282828',
+                                }}
+                                placeholder={"Author's Name (Optional)"}
+                                placeholderTextColor={colors.secondary_text}
+                                onChangeText={setAuthor}
+                                value={author}
+                            />
+                        </View>
+                    </View>
+                </TouchableWithoutFeedback>
+            </KeyboardAvoidingView>
         </Screen>
     );
 };
