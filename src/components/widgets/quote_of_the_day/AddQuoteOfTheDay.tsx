f50c0e057242ad52c1ce7021412bdf2fc9f2dbc9
stronkbad <37816659+stronkbad@users.noreply.github.com>
2023-12-26 06:41:57 -0500

    test

--- src/components/widgets/quote_of_the_day/AddQuoteOfTheDay.tsx
+++ src/components/widgets/quote_of_the_day/AddQuoteOfTheDay.tsx
@@ -1,15 +1,16 @@
 import { useNavigation } from '@react-navigation/native';
 import React from 'react';
-import { Keyboard, KeyboardAvoidingView, ScrollView, Text, View } from 'react-native';
-import { TextInput, TouchableWithoutFeedback } from 'react-native';
+import { KeyboardAvoidingView, Text, View } from 'react-native';
+import { TextInput } from 'react-native';
 import { Banner } from 'src/components/common/Banner';
 import { Screen } from 'src/components/common/Screen';
 import { useTheme } from 'src/components/theme/ThemeProvider';
-import { POPPINS_REGULAR } from 'src/util/constants';
-import { isIosApp } from 'src/util/DeviceUtil';
+import { POPPINS_REGULAR, TIMELINE_CARD_PADDING } from 'src/util/constants';
 import { StackNavigationProp } from '@react-navigation/stack';
 import { TodayTab } from 'src/navigation/RootStackParamList';
 import { QuoteOfTheDayController } from 'src/controller/widget/quote_of_the_day/QuoteOfTheDayController';
+import { isIosApp } from 'src/util/DeviceUtil';
+import { TouchableWithoutFeedback, Keyboard } from 'react-native';
 
 export const AddQuoteOfTheDay = () => {
     const { colors } = useTheme();
@@ -24,6 +25,28 @@ export const AddQuoteOfTheDay = () => {
         navigation.goBack();
     };
 
+    const styles = {
+        container: {
+            backgroundColor: '#282828',
+            color: colors.text,
+            padding: TIMELINE_CARD_PADDING,
+            borderRadius: 5,
+            fontFamily: POPPINS_REGULAR,
+        },
+        layout: {
+            flex: 1,
+            padding: 20,
+        },
+        text: {
+            color: colors.text,
+            fontFamily: POPPINS_REGULAR,
+            fontSize: 13,
+        },
+        header: {
+            fontSize: 15,
+        }
+    };
+
     return (
         <Screen>
             <Banner
@@ -32,96 +55,61 @@ export const AddQuoteOfTheDay = () => {
                 leftRoute="BACK"
                 rightText="save"
                 rightOnClick={save}
+                rightEnabled={quote.length > 0}
+                rightColor={quote.length > 0 ? colors.link : 'gray'}
             />
-            <ScrollView style={{ width: '100%', height: '100%' }}>
-                <KeyboardAvoidingView
-                    style={{ height: '100%' }}
-                    keyboardVerticalOffset={isIosApp() ? -10 : 111}
-                    behavior={isIosApp() ? 'padding' : 'height'}
-                >
-                    <TouchableWithoutFeedback
-                        style={{ height: '100%', alignItems: 'center' }}
-                        onPress={() => {
-                            Keyboard.dismiss();
-                        }}
-                    >
-                        <View style={{ width: '95%', paddingLeft: 5, paddingTop: 20 }}>
-                            <View>
-                                <Text
-                                    style={{
-                                        fontFamily: POPPINS_REGULAR,
-                                        color: colors.text,
-                                        fontSize: 12,
-                                        paddingTop: 5,
-                                    }}
-                                >
-                                    The Quote Of The Day is a community supplied quote pool where
-                                    one quote is selected per day as the Quote Of The Day.
-                                </Text>
-                                <Text
-                                    style={{
-                                        fontFamily: POPPINS_REGULAR,
-                                        color: colors.text,
-                                        fontSize: 12,
-                                        paddingTop: 10,
-                                    }}
-                                >
-                                    Add a new quote to join in on the fun - you might be next!
-                                </Text>
-                            </View>
-                            <View style={{ height: 30, width: '100%' }} />
 
-                            <View style={{ width: '100%' }}>
-                                <Text style={{ fontFamily: POPPINS_REGULAR, color: colors.text }}>
-                                    Quote
+            <KeyboardAvoidingView style={{ flex: 1 }} behavior={isIosApp() ? 'padding' : 'height'}>
+                <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+                    <View style={[styles.layout, { justifyContent: 'flex-start', marginBottom: 25 }]}>
+                        <View style={{ paddingBottom: 40 }}>
+                            <View style={[styles.container]}>
+                                <Text
+                                    style={[ styles.text]}>
+                                    Add a quote that inspires you! Once a day we randomly select a
+                                    quote to inspire the entire community.{' '}
+                                    <Text
+                                        style={{ color: colors.accent_color_light}}
+                                    >
+                                        Yours could be next!
+                                    </Text>
                                 </Text>
-                                <TextInput
-                                    textAlignVertical="top"
-                                    style={{
-                                        marginTop: 3,
-                                        width: '100%',
-                                        height: 200,
-                                        borderRadius: 12,
-                                        backgroundColor: colors.text_input_background,
-                                        borderColor: colors.text_input_border,
-                                        borderWidth: 1,
-                                        color: colors.text,
-                                        paddingTop: 10,
-                                        paddingLeft: 10,
-                                        paddingRight: 10,
-                                    }}
-                                    multiline={true}
-                                    placeholder={'add a quote that inspires you!'}
-                                    placeholderTextColor={colors.secondary_text}
-                                    onChangeText={setQuote}
-                                    value={quote}
-                                />
-                            </View>
-
-                            <View style={{ width: '100%', paddingTop: 10 }}>
-                                <Text style={{ fontFamily: POPPINS_REGULAR, color: colors.text }}>
-                                    Author
-                                </Text>
-                                <TextInput
-                                    style={{
-                                        marginTop: 3,
-                                        padding: 15,
-                                        color: colors.text,
-                                        borderRadius: 12,
-                                        backgroundColor: colors.text_input_background,
-                                        borderColor: colors.text_input_border,
-                                        borderWidth: 1,
-                                    }}
-                                    placeholder={"Author's Name (Optional)"}
-                                    placeholderTextColor={colors.secondary_text}
-                                    onChangeText={setAuthor}
-                                    value={author}
-                                />
                             </View>
                         </View>
-                    </TouchableWithoutFeedback>
-                </KeyboardAvoidingView>
-            </ScrollView>
+
+                        <View style={{ flexDirection: 'row' }}>
+                            <Text style={[styles.text,styles.header]}>
+                                Quote
+                            </Text>
+                        </View>
+
+                        <View style={{ paddingBottom: 20 }}>
+                            <TextInput
+                                textAlignVertical="top"
+                                style={[styles.container, {height: 120}]}
+                                multiline={true}
+                                placeholder={'add a quote that inspires you!'}
+                                placeholderTextColor={colors.secondary_text}
+                                onChangeText={setQuote}
+                                value={quote}
+                            />
+                        </View>
+
+                        <View style={{ paddingTop: 10 }}>
+                            <Text style={[styles.text,styles.header]}>
+                                Author
+                            </Text>
+                            <TextInput
+                                style={[styles.container]}
+                                placeholder={"Author's Name (Optional)"}
+                                placeholderTextColor={colors.secondary_text}
+                                onChangeText={setAuthor}
+                                value={author}
+                            />
+                        </View>
+                    </View>
+                </TouchableWithoutFeedback>
+            </KeyboardAvoidingView>
         </Screen>
     );
 };
