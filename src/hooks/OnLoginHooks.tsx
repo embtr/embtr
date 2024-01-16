e5517f6a88ddb6064ab84bfaa01d481c0683b826
thedevdad <brent@embtr.com>
2024-01-16 04:25:47 -0500

    EMBTR-23 - refactored onLogin Hook logic

--- src/hooks/OnLoginHooks.tsx
+++ src/hooks/OnLoginHooks.tsx
@@ -8,58 +8,65 @@ import { UpdateUtility } from 'src/util/updates/UpdateUtility';
 import { useAppSelector } from 'src/redux/Hooks';
 import { getAcknowledgedVersion } from 'src/redux/user/GlobalState';
 
-export namespace OnLoginHooks {
-    export const useOnLogin = () => {
-        const navigation = useEmbtrNavigation();
+const handleLoginModals = () => {
+    const navigation = useEmbtrNavigation();
 
-        const currentUser = UserCustomHooks.useCurrentUser();
-        const termsVersion = MetadataCustomHooks.useLatestTermsVersion();
+    const currentUser = UserCustomHooks.useCurrentUser();
+    const termsVersion = MetadataCustomHooks.useLatestTermsVersion();
 
-        const minimumAppVersion = MetadataCustomHooks.useMinimumAppVersion();
-        const latestAppVersion = MetadataCustomHooks.useLatestAppVersion();
-        const currentVersion = Constants.expoConfig?.version;
-        const acknowledgeVersion = useAppSelector(getAcknowledgedVersion);
+    const minimumAppVersion = MetadataCustomHooks.useMinimumAppVersion();
+    const latestAppVersion = MetadataCustomHooks.useLatestAppVersion();
+    const currentVersion = Constants.expoConfig?.version;
+    const acknowledgeVersion = useAppSelector(getAcknowledgedVersion);
 
-        if (currentUser.data && !currentUser.data?.accountSetup) {
-            navigation.navigate(Routes.NEW_USER_PROFILE_POPULATION);
+    if (currentUser.data && !currentUser.data?.accountSetup) {
+        navigation.navigate(Routes.NEW_USER_PROFILE_POPULATION);
+        return;
+    }
+
+    if (currentUser.data && termsVersion.data) {
+        const userTermsVersion = currentUser.data.termsVersion ?? 0;
+        const termsVersionNumber = Number(termsVersion.data);
+
+        if (userTermsVersion < termsVersionNumber) {
+            navigation.navigate(Routes.TERMS_APPROVAL_MODAL);
+            return;
+        }
+    }
+
+    if (currentVersion && minimumAppVersion.data && latestAppVersion.data) {
+        const softUpdateAvailable = UpdateUtility.updateIsAvailable(
+            currentVersion,
+            latestAppVersion.data
+        );
+        const softUpdateAcknowledged = !UpdateUtility.updateIsAvailable(
+            acknowledgeVersion,
+            latestAppVersion.data
+        );
+        const hardUpdateAvailable = UpdateUtility.updateIsAvailable(
+            currentVersion,
+            minimumAppVersion.data
+        );
+
+        if (hardUpdateAvailable) {
+            console.log('Hard update available, navigating to update modal');
+            navigation.navigate(Routes.UPDATE_AVAILABLE_MODAL);
             return;
         }
 
-        if (currentUser.data && termsVersion.data) {
-            const userTermsVersion = currentUser.data.termsVersion ?? 0;
-            const termsVersionNumber = Number(termsVersion.data);
-
-            if (userTermsVersion < termsVersionNumber) {
-                navigation.navigate(Routes.TERMS_APPROVAL_MODAL);
-                return;
-            }
+        if (softUpdateAvailable && !softUpdateAcknowledged) {
+            console.log('Hard update available, navigating to update modal');
+            navigation.navigate(Routes.UPDATE_AVAILABLE_MODAL);
+            return;
         }
+    }
+};
 
-        if (currentVersion && minimumAppVersion.data && latestAppVersion.data) {
-            const softUpdateAvailable = UpdateUtility.updateIsAvailable(
-                currentVersion,
-                latestAppVersion.data
-            );
-            const softUpdateAcknowledged = !UpdateUtility.updateIsAvailable(
-                acknowledgeVersion,
-                latestAppVersion.data
-            );
-            const hardUpdateAvailable = UpdateUtility.updateIsAvailable(
-                currentVersion,
-                minimumAppVersion.data
-            );
+const handlePushNotificationToken = () => {};
 
-            if (hardUpdateAvailable) {
-                console.log('Hard update available, navigating to update modal');
-                navigation.navigate(Routes.UPDATE_AVAILABLE_MODAL);
-                return;
-            }
-
-            if (softUpdateAvailable && !softUpdateAcknowledged) {
-                console.log('Hard update available, navigating to update modal');
-                navigation.navigate(Routes.UPDATE_AVAILABLE_MODAL);
-                return;
-            }
-        }
+export namespace OnLoginHooks {
+    export const useOnLogin = () => {
+        handleLoginModals();
+        handlePushNotificationToken();
     };
 }
