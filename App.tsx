9b5a72e0572d5a2ec3704cd0317770c1f8f8380f
thedevdad <brent@embtr.com>
2024-01-21 05:08:26 -0500

    adding sentry

--- App.tsx
+++ App.tsx
@@ -10,6 +10,7 @@ import { RootSiblingParent } from 'react-native-root-siblings';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { LogBox } from 'react-native';
+import * as Sentry from '@sentry/react-native';
 
 /*
  * "ABANDON ALL HOPE YE WHO ENTER HERE" - loganmbutler - 2023-11-29
