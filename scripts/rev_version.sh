8cd635259db9f15312c1f390cbc17360ea288b5d
thedevdad <brent@embtr.com>
2024-02-03 05:04:25 -0500

    saving dry run

--- scripts/rev_version.sh
+++ scripts/rev_version.sh
@@ -2,6 +2,12 @@
 
 pushd ..
 
+dry=0
+if [[ "$1" == "--dry" ]]; then 
+    echo "dry run"
+    dry=1
+fi
+
 currentVersion="$(cat app.json | grep -o '"buildNumber": .*' | cut -d' ' -f2 | sed 's/"//g' | tr -d '[:space:]')"
 currentVersionCode="$(cat app.json | grep -o '"versionCode": [0-9]*' | cut -d' ' -f2)"
 
@@ -13,9 +19,11 @@ read -r versionCode <<< "$(echo "$currentVersionCode")"
 ((versionCode++))
 newVersionCode="$versionCode"
 
-sed -i "s/\"$currentVersion/\"$newVersion/g" package.json
-sed -i "s/\"$currentVersion/\"$newVersion/g" app.json
-sed -i "s/$currentVersionCode/$newVersionCode/g" app.json
+if [[ dry -eq 0 ]]; then 
+    sed -i "s/\"$currentVersion/\"$newVersion/g" package.json
+    sed -i "s/\"$currentVersion/\"$newVersion/g" app.json
+    sed -i "s/$currentVersionCode/$newVersionCode/g" app.json
+fi
 
 echo "$currentVersion => $newVersion"
 echo "$currentVersionCode => $newVersionCode"
