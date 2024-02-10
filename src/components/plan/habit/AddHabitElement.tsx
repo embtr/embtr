2850ac67e41a8a6bb0aade423aca56080273bddb
stronkbad <37816659+stronkbad@users.noreply.github.com>
2024-02-10 09:23:24 -0500

    Update AddHabitElement.tsx

--- src/components/plan/habit/AddHabitElement.tsx
+++ src/components/plan/habit/AddHabitElement.tsx
@@ -31,7 +31,7 @@ export const AddHabitElement = ({
             <View
                 style={[
                     {
-                        backgroundColor: colors.background_light,
+                        backgroundColor: highlightElement ? colors.accent_color_dim : colors.background_light,
                         borderRadius: 9,
                         flex: 1,
                         flexDirection: 'row',
