d9500cb000bc7496c4f980cbe78eab3d66a69655
thedevdad <brent@embtr.com>
2024-02-23 05:01:53 -0500

    removing image compression

--- src/util/ImagePickerUtil.tsx
+++ src/util/ImagePickerUtil.tsx
@@ -3,8 +3,8 @@ import * as ImagePicker from 'expo-image-picker';
 export const pickImage = async (): Promise<ImagePicker.ImagePickerResult> => {
     let pickerResult = await ImagePicker.launchImageLibraryAsync({
         allowsEditing: true,
-        quality: 0,
         aspect: [4, 3],
+        // quality: 0.175,
     });
 
     return pickerResult;
@@ -15,7 +15,7 @@ export const pickImages = async (): Promise<ImagePicker.ImagePickerResult> => {
         allowsEditing: true,
         allowsMultipleSelection: true,
         aspect: [4, 3],
-        quality: 0,
+        // quality: 0.175,
     });
 
     return pickerResult;
