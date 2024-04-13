96cfceddc7ab8af274629b7301e1dda8695b9542
thedevdad <brent@embtr.com>
2024-04-13 05:04:33 -0400

    fixing revenuecat issue

--- src/controller/revenuecat/RevenueCatProvider.tsx
+++ src/controller/revenuecat/RevenueCatProvider.tsx
@@ -1,10 +1,10 @@
 import { RevenueCat } from './RevenueCat';
 import { RevenueCatDummy } from './RevenueCatDummy';
-//import { RevenueCatImpl } from './RevenueCatImpl';
+import { RevenueCatImpl } from './RevenueCatImpl';
 
 export class RevenueCatProvider {
     public static get(): RevenueCat {
-        //return new RevenueCatImpl();
-        return new RevenueCatDummy();
+        //return new RevenueCatDummy();
+        return new RevenueCatImpl();
     }
 }
