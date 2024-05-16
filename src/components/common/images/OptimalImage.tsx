fe09353a287b2803dcd92e5e48efcb84a9a4fec7
thedevdad <brent@embtr.com>
2024-05-16 05:19:52 -0400

    added milestones to planneddayresults

--- src/components/common/images/OptimalImage.tsx
+++ src/components/common/images/OptimalImage.tsx
@@ -2,10 +2,15 @@ import { Image, ImageStyle, StyleProp, View } from 'react-native';
 import { LocalImageRepo } from './LocalImageRepo';
 import { Image as ExpoImage } from 'expo-image';
 import { isIosApp } from 'src/util/DeviceUtil';
+import { Ionicons } from '@expo/vector-icons';
 
 export interface OptimalImageData {
     localImage?: string;
     remoteImageUrl?: string;
+    ionicon?: {
+        name: string;
+        color: string;
+    };
 }
 
 interface Props {
@@ -24,6 +29,15 @@ export const OptimalImage = ({ data, style }: Props) => {
 
     if (data.remoteImageUrl) {
         return <ExpoImage source={{ uri: data.remoteImageUrl }} style={style} />;
+    }
+
+    if (data.ionicon) {
+        // @ts-ignore
+        const height = style?.height ?? 0;
+
+        return (
+            <Ionicons size={height} color={data.ionicon.color} name={data.ionicon.name as any} />
+        );
     }
 
     return <View />;
