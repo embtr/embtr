c005a076de32fce142f27433dd7415943873b843
thedevdad <thedevdad@war-machine.local>
2024-07-28 16:55:42 -0400

    fixing crash

--- src/components/common/badge/BadgeBelt.tsx
+++ src/components/common/badge/BadgeBelt.tsx
@@ -11,11 +11,11 @@ interface Props {
 // "Oopsy this is all wrong need to update" - LtConquer - 2024-06-19
 
 export const BadgeBelt = ({ user, size }: Props) => {
-    const badges = user.userBadges ?? [];
-    badges.sort((a, b) => (a.badge?.priority ?? 0) - (b.badge?.priority ?? 0));
+    const badges = [...(user.userBadges ?? [])];
+    const sortedBadges = badges.sort((a, b) => (a.badge?.priority ?? 0) - (b.badge?.priority ?? 0));
 
     const badgeElements = [];
-    for (const badge of user.userBadges ?? []) {
+    for (const badge of sortedBadges ?? []) {
         badgeElements.push(
             <View style={{ paddingRight: PADDING_SMALL / 3 }}>
                 <UserBadge key={badge.userId + '_' + badge.id} size={size} userBadge={badge} />
